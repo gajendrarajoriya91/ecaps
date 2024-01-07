89aa6ccefdd8277a0f5619948d261db52299a26e
gajendrarajoriya91 <gajendraprofe987@gmail.com>
2024-01-07 20:27:21 +0530

    Minor change

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,27 +1,23 @@
-const winston = require('winston');
+const winston = require("winston");
 const { createLogger, format, transports } = winston;
 const { combine, timestamp, printf } = format;
-const DailyRotateFile = require('winston-daily-rotate-file'); 
+const DailyRotateFile = require("winston-daily-rotate-file");
 
 const logFormat = printf(({ level, message, timestamp }) => {
   return `${timestamp} ${level}: ${message}`;
 });
 
 const logger = createLogger({
-  format: combine(
-
-    timestamp({ format: 'DD-MM-YYYY HH:mm:ss' }), 
-    logFormat
-  ),
+  format: combine(timestamp({ format: "DD-MM-YYYY HH:mm:ss" }), logFormat),
   transports: [
-    new transports.Console(), 
+    new transports.Console(),
     new DailyRotateFile({
-      dirname: 'logs',
-      filename: 'application-%DATE%.log',
-      datePattern: 'DD-MM-YYYY',
+      dirname: "logs",
+      filename: "application-%DATE%.log",
+      datePattern: "DD-MM-YYYY",
       zippedArchive: true,
-      maxSize: '20m',
-      maxFiles: '14d',
+      maxSize: "20m",
+      maxFiles: "14d",
     }),
   ],
 });
